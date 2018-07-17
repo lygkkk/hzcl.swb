a9a86f6f1be3bc275903c074d98608f8127f0e07
Administrator <Administrator@DEEP-1806110807>
2018-07-17 17:03:11 +0800

    Cache

---
+++ WebApplication1/6.6/CacheDemo.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using hzcl.swb.BLL;
+
+namespace WebApplication1._6._6
+{
+    public partial class CacheDemo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Cache["customerInfo"] == null)
+            {
+                customerBll customerBll = new customerBll();
+                Cache["customerInfo"] = customerBll.GetAllCustomerInfo();
+                Response.Write("来自数据库的数据");
+            }
+            else
+            {
+                DataTable dt = (DataTable) Cache["customerInfo"];
+            }
+        }
+    }
+}
