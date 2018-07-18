e05533a081d478905bb27c0ef498d9c218455c2b
Administrator <Administrator@DEEP-1806110807>
2018-07-18 15:18:52 +0800

    2018.7.18.15.18

--- WebApplication1/6.6/CacheDemo.aspx.cs
+++ WebApplication1/6.6/CacheDemo.aspx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Web;
+using System.Web.Caching;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using hzcl.swb.BLL;
@@ -11,17 +12,27 @@ namespace WebApplication1._6._6
 {
     public partial class CacheDemo : System.Web.UI.Page
     {
+        
         protected void Page_Load(object sender, EventArgs e)
         {
             if (Cache["customerInfo"] == null)
             {
                 customerBll customerBll = new customerBll();
-                Cache["customerInfo"] = customerBll.GetAllCustomerInfo();
+                Cache.Insert("customerInfo", customerBll.GetAllCustomerInfo(), null, DateTime.Now.AddSeconds(3), TimeSpan.Zero, CacheItemPriority.Normal, RemoveCacheItem);
                 Response.Write("来自数据库的数据");
             }
             else
             {
-                DataTable dt = (DataTable) Cache["customerInfo"];
+                Response.Write("来缓存");
+                //DataTable dt = (DataTable) Cache["customerInfo"];
+            }
+        }
+
+        protected void RemoveCacheItem(string key, object value, CacheItemRemovedReason reason)
+        {
+            if (reason == CacheItemRemovedReason.Expired)
+            {
+                //这里写移除的日志到硬盘
             }
         }
     }
